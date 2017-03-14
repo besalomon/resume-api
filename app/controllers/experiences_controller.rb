468dca987f8d93eaa7f68077f9cf9529e4712a7a
Salomon Beza <beza.salomon@gmail.com>
2017-03-14 12:37:25 +0200

    changed experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -8,7 +8,7 @@ class ExperiencesController < ApplicationController
 
   def create
     @experience = Experience.create(start_date: params[:start_date], end_date: params[:end_date], job_title: params[:job_title], company_name: params[:company_name], details: params[:details], student_id: params[:student_id])
-    render: @student
+    render json: @experience
   end
 
   def update
@@ -16,12 +16,12 @@ class ExperiencesController < ApplicationController
     @experience.update(
       start_date: params[:start_date], end_date: params[:end_date], job_title: params[:job_title], company_name: params[:company_name], details: params[:details]
       )
-    render: show
+    render :show
   end
 
   def destroy
     @experience = Experience.find_by(id:params[:id])
     @experience.destroy
-    render: index
+    render:index
   end
 end
