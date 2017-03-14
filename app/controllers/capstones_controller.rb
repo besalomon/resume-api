4542570e36683e3c0ec0521bfb5afc8078f80f8c
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-03-14 11:56:06 +0200

    created controller actions and views

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,2 +1,43 @@
 class CapstonesController < ApplicationController
+
+  def index 
+    @capstones = Capstone.all     
+  end
+
+  def new 
+    
+  end
+
+  def create 
+    name = params[:name],
+    description = params[:description],
+    url = params[:url],
+    screenshot = params[:screenshot]
+    capstone = Capstone.create({name: name, description: description, url: url, screenshot: screenshot, student_id: student_id})
+
+    render :show
+  end
+
+  def edit 
+    @capstone = Capstone.find_by(id: params[:id])
+  end
+
+  def update 
+    capstone = Capstone.find_by(id: params[:id])
+    @capstone = Capstone.update(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot: [:screenshot]
+      )
+    render jason: @capstone 
+
+  end
+
+   def destroy
+    @capstone = Capstone.find_by(id:params[:id])
+    @capstone.destroy
+
+    render json: {message: "Team Removed!!!!!"}
+  end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,6 @@
 class SkillsController < ApplicationController
+
+  def index
+    skills = Skill.all     
+  end
 end
