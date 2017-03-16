59346792b3ed8d706b3e376eb8bc35336d69d1ab
Salomon Beza <beza.salomon@gmail.com>
2017-03-16 10:48:02 +0200

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,12 @@ Education.create!([
   {start_date: "2015-04-12", end_date: "2016-04-12", degree: "Associate of Arts", university_name: "Southern New Hampshire", student_id: 1, details: "Graduated my AA and working for my AA"}
 ])
 Experience.create!([
-  {start_date: "2016-01-25", end_date: "2016-06-25", job_title: "Tech Assistant", company_name: "Kepler", details: "Worked with the technician to solve computer issues for students and staff members", student_id: 1},
   {start_date: "2016-01-25", end_date: "2016-06-25", job_title: "Tech Assistant", company_name: "Kepler", details: "Worked with the technician to solve computer issues for students and staff members", student_id: 1}
 ])
 Skill.create!([
-  {name: "Customer Service", student_id: 1}
+  {name: "Customer Service", student_id: 1},
+  {name: "JavaScript", student_id: 1}
 ])
 Student.create!([
-  {first_name: "Eugene", last_name: "Niyitanga", email: "eugene.niyitanga2015@kepler.org", phone_number: "0785607577", short_bio: "Student at Kepler. Likes football a lot! Full-Stack web developper", linkedin_url: "https://www.linkedin.com/in/eugene-niyitanga-a3a349101/", twitter_handle: "https://twitter.com/eugeneniyitang1", blog_url: "http://niyitangaeugene.blogspot.com/", resume_url: "", github_url: "https://github.com/eugeneniyitanga", photo: "https://lh3.googleusercontent.com/-Ikg5h6PD-rk/AAAAAAAAAAI/AAAAAAAAAA8/-uywAo66Lrk/photo.jpg"},
-  {first_name: "Eugene", last_name: "Niyitanga", email: "eugene.niyitanga2015@kepler.org", phone_number: "0785607577", short_bio: "Student at Kepler. Likes football a lot! Full-Stack web developper", linkedin_url: "https://www.linkedin.com/in/eugene-niyitanga-a3a349101/", twitter_handle: "https://twitter.com/eugeneniyitang1", blog_url: "http://niyitangaeugene.blogspot.com/", resume_url: "", github_url: "https://github.com/eugeneniyitanga", photo: "https://lh3.googleusercontent.com/-Ikg5h6PD-rk/AAAAAAAAAAI/AAAAAAAAAA8/-uywAo66Lrk/photo.jpg"}
+  {first_name: "Eugene", last_name: "Niyitanga", email: "eugene.niyitanga2015@kepler.org", phone_number: "0785607577", short_bio: "Student at Kepler. Likes football a lot! Full-Stack web developper", linkedin_url: "https://www.linkedin.com/in/eugene-niyitanga-a3a349101/", twitter_handle: "https://twitter.com/eugeneniyitang1", blog_url: "http://niyitangaeugene.blogspot.com/", resume_url: "", github_url: "https://github.com/eugeneniyitanga", photo: "https://lh3.googleusercontent.com/-Ikg5h6PD-rk/AAAAAAAAAAI/AAAAAAAAAA8/-uywAo66Lrk/photo.jpg", password_digest: "$2a$10$3nZYU.RvYT0zquj2AzAAJupn5YouXifIKZZ1isTRMJNA6dDUOeNd2"}
 ])
